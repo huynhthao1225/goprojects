9e9dff08f5006320a22f0a7c5a3563f5b1aa5b87
huynh <thao.huynh@outlook.com>
2019-10-07 20:10:54 -0400

    added channel to write to console

--- src/mysqlsample/main.go
+++ src/mysqlsample/main.go
@@ -6,6 +6,7 @@ import (
 	"mysqlsample/dao"
 	"mysqlsample/dbcon"
 	"mysqlsample/utils"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -20,7 +21,7 @@ func main() {
 	if len(conProperties.ConnectionProperties) == 1 {
 		conProperty = conProperties.ConnectionProperties[0]
 	}
-	
+
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
@@ -29,7 +30,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	} else {
-		fmt.Println("db connection is a GO ...")		
+		fmt.Println("db connection is a GO ...")
 	}
 
 	actorAction := dao.ActionActor{}
@@ -40,14 +41,30 @@ func main() {
 
 	actors := actorAction.FindAll(db)
 
+	jobs := make(chan string, 5)
+	fp, _ := os.OpenFile("data.txt", os.O_RDWR|os.O_CREATE, 0755)
+	go writeToConsole(jobs)
 	for _, actor := range actors {
-		fmt.Printf("%d\t%s %s\t\t%s\n", actor.ID, actor.FirstName, actor.LastName, actor.LastUpdate)
+		value := fmt.Sprintf("%d\t%s %s\t\t%s\n", actor.ID, actor.FirstName, actor.LastName, actor.LastUpdate)
+		jobs <- value
+		fp.WriteString(fmt.Sprintf("%d\t%s %s\t\t%s\n", actor.ID, actor.FirstName, actor.LastName, actor.LastUpdate))
 	}
-
+	close(jobs)
+	
 	addressAction := dao.ActionAddress{}
 	address := addressAction.FindbyID(1, db)
 	fmt.Println(address.ID, address.Address, address.Location)
-	fmt.Printf("ConnectionString = %s\n",connectionString)
-	
+	fmt.Printf("ConnectionString = %s\n", connectionString)
 
+}
+
+func writeToConsole(ch <-chan string) {
+	for {
+		value, more  := <- ch
+		if more {
+			fmt.Print(value)
+		} else {
+			return
+		}
+	}
 }
